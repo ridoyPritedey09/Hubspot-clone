a9ea18a475f214de6374ad426ff755cf8f92cdf4
Ridoy Chandra Dey <RidoyChandraDey09@gmail.com>
2021-10-20 14:50:05 +0600

    upper part of the scrolling nav all done

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@
 const search_bounding_box = document.querySelector("#search-bounding-box");
 const search_icon = document.querySelector("#search-icon");
 const search_input = document.querySelector("#search-input");
-const search_icon_width = search_icon.getBoundingClientRect().width+3;
+const search_icon_width = search_icon.getBoundingClientRect().width + 3;
 
 search_icon.addEventListener("click", (e) => {
     search_icon.style.cssText = "border:0;outline:none;";
     search_bounding_box.style.cssText = "border: 1px solid var(--ocean-blue);box-shadow: 0 0 4px 1px rgba(0, 208, 228, 0.3), 0 0 0 1px #00d0e4;";
-    search_input.style.cssText = "width:calc(100% - "+search_icon_width+"px);";
+    search_input.style.cssText = "width:calc(100% - " + search_icon_width + "px);";
 });
 
 search_input.addEventListener("blur", (e) => {
@@ -18,41 +18,94 @@ search_input.addEventListener("blur", (e) => {
 
 
 
+// these code down below are work for upperside of the scroll navigation web version start
+
 // hover items for language nav item and about nav item
 
 // selected element
-const hs_dropdown_menu = document.querySelector(".hs-dropdown_menu");
-const nav_link = document.querySelector(".nav-link");
+const hs_dropdown_menu_all = document.querySelectorAll(".hs-dropdown_menu");
+const nav_link_hover_all = document.querySelectorAll(".hs-dr-nav-link_hover");
 const nav_link_wrapper = document.querySelector(".hs-nav-link_wrapper");
-const dropdown_icon = document.querySelector(".dropdown-icon");
-const link_label_icon = document.querySelector(".link-label-icon");
-const get_css_style_before = window.getComputedStyle(nav_link_wrapper,"::before");
-const get_css_style_after = window.getComputedStyle(nav_link_wrapper,"::after");
+const nav_link_wrapper_all = document.querySelectorAll(".hs-nav-link_wrapper");
+const dropdown_icon_all = document.querySelectorAll(".dropdown-icon");
+const link_label_icon_all = document.querySelectorAll(".hs-label_hover");
+const get_css_style_before = window.getComputedStyle(nav_link_wrapper, "::before");
+const get_css_style_after = window.getComputedStyle(nav_link_wrapper, "::after");
 
-// nav hover items toogle for before and after angle
-hs_dropdown_menu.addEventListener("mouseover",(e)=>{
-    nav_link_wrapper.style.setProperty("--rotateX",'rotateX(90deg)');
+
+
+// nav hover items toggle for before and after angle (start)
+hs_dropdown_menu_all.forEach(item => {
+    item.addEventListener("mouseover", (e) => {
+        nav_link_wrapper_all.forEach(item => {
+            item.style.setProperty("--rotateX", 'rotateX(90deg)');
+        })
+    });
 });
 
-hs_dropdown_menu.addEventListener("mouseout",(e)=>{
-    nav_link_wrapper.style.setProperty("--rotateX",'rotateX(0deg)');
+hs_dropdown_menu_all.forEach(item => {
+    item.addEventListener("mouseout", (e) => {
+        nav_link_wrapper_all.forEach(item => {
+            item.style.setProperty("--rotateX", 'rotateX(0deg)');
+        })
+    });
 });
+// nav hover items toggle for before and after angle (end)
 
+
+
+// resuseable funciton for hs-dropdown_menu style
+// making some array for better usecase 
+const dropdown_menu_array = Array.from(hs_dropdown_menu_all);
+const nav_link_hover_array = Array.from(nav_link_hover_all);
+const dropdown_icon_array = Array.from(dropdown_icon_all);
+const link_label_array = Array.from(link_label_icon_all);
+
+// contain the same index nuber for all hover state 
+let parentIndex = '';
+
+// reuseable code for mouseover and mouseout state 
+
+function changeStyles(color,decor) {
+    nav_link_hover_array[parentIndex].style.color = color;
+    nav_link_hover_array[parentIndex].style.textDecoration = decor;
+    dropdown_icon_array[parentIndex].style.fill = color;
+    if(link_label_array[parentIndex] == null){
+        return ;
+    }else{
+        link_label_array[parentIndex].style.fill = color;
+    }
+    
+}
 
 // current nav item color will appear when mouse is hover the dropdown menu items
-hs_dropdown_menu.addEventListener("mouseover",(e)=>{
-    nav_link.style.color='rgb(0, 145, 174)';
-    dropdown_icon.style.fill='rgb(0, 145, 174)';
-    link_label_icon.style.fill='rgb(0, 145, 174)';
-    
-});
+dropdown_menu_array.forEach((item, index, arr) => {
 
-hs_dropdown_menu.addEventListener("mouseout",(e)=>{
-    nav_link.style.color='';
-    dropdown_icon.style.fill='';
-    link_label_icon.style.fill='';
-    
-});
+    item.addEventListener("mouseover", (e) => {
+        if (arr[index] == e.target.closest(".hs-dropdown_menu")) {
+            parentIndex = index;
+            changeStyles('rgb(0, 145, 174)','underline');
+        } else {
+            return;
+        }
 
+    })
+})
+
+dropdown_menu_array.forEach((item, index, arr) => {
+
+    item.addEventListener("mouseout", (e) => {
+        if (arr[index] == e.target.closest(".hs-dropdown_menu")) {
+            parentIndex = index;
+            changeStyles('','');
+        } else {
+            return;
+        }
+
+    })
+})
+
+// these code up above are work for upperside of the scroll navigation web version end.
 
 // hover items for the nav link end 
+
